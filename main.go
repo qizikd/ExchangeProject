587570f00676117cd51033b8d5f8e613168647b2
qizikd <qizikd@163.com>
2018-10-30 11:30:35 +0800

    解析参数

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	port := flag.String("port", "80", "Listen port")
+	flag.Parse()
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, World!")
