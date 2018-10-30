bdf9be0e2f26c893919c58636ce24320f6ee1ce9
qizikd <qizikd@163.com>
2018-10-30 11:23:14 +0800

    修改端口号

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"flag"
 	"github.com/ExchangeProject/user"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
+	port := flag.String("port", "80", "Listen port")
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, World!")
 	})
 	router.GET("/user/create", user.New)
-	router.Run(":81")
+	router.Run(":" + *port)
 }
