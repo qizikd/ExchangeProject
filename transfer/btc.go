13e119a0942639c848eb763fbd9770b7f37b2b38
qizikd <qizikd@163.com>
2018-11-19 18:16:54 +0800

    记录导入私钥用时

--- transfer/btc.go
+++ transfer/btc.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type GobcyAddInfo struct {
@@ -226,5 +227,8 @@ func ImportPrivkey(privkey string, label string) (err error) {
 		glog.Error(err)
 		return
 	}
-	return client.ImportPrivKeyRescan(wif, label, false)
+	fmt.Println(time.Now(), "私钥导入开始")
+	err = client.ImportPrivKeyRescan(wif, label, true)
+	fmt.Println(time.Now(), "私钥导入结束")
+	return err
 }
