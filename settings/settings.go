2fa24d09098c8f2dc7c5724c693eb75c4d9ea578
qizikd <qizikd@163.com>
2018-11-23 17:37:05 +0800

    btc改为正式链

--- settings/settings.go
+++ settings/settings.go
@@ -5,5 +5,5 @@ import "database/sql"
 var (
 	MySqlSourceName = "exchange:sSfwRTwp4Lnp5CM2@tcp(39.104.156.29:3306)/exchange"
 	DBMysql         *sql.DB
-	IsBTCTestNet3   = true
+	IsBTCTestNet3   = false
 )
